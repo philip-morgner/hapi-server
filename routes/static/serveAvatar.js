a0a4a5dd138fa4dce26153f9196938037a164f24
Philip Morgner <p.morgner@searchmetrics.com>
2019-04-17 19:10:15 +0200

    default avatar

--- routes/static/serveAvatar.js
+++ routes/static/serveAvatar.js
@@ -1,10 +1,14 @@
 "use strict";
 
 import glob from "glob";
+import Path from "path";
 import { verifyUserExists } from "../../middleware/verification";
 import { findFile } from "../../middleware/user";
 
 const PUBLIC_DIR = "public/";
+const DEFAULT_PATH = Path.join(
+  process.env.path + "../../public/default_avatar.png"
+);
 
 exports.plugin = {
   name: "avatar",
@@ -22,6 +26,9 @@ exports.plugin = {
         const { user_id } = request.params;
         const path = await findFile(user_id, PUBLIC_DIR);
 
+        if (!path) {
+          return h.file(DEFAULT_PATH);
+        }
         return h.file(path);
       }
     });
